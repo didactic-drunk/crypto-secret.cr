de11459cb1d71564230c922a1b1bbffa8e5a2231
Didactic Drunk <1479616+didactic-drunk@users.noreply.github.com>
2021-06-13 12:42:14 -0700

    Crypto::Secret Add protection method stubs

--- src/crypto-secret/secret.cr
+++ src/crypto-secret/secret.cr
@@ -1,6 +1,8 @@
 # Interface to hold sensitive information (often cryptographic keys)
 #
+# **Only for direct use by cryptographic library authors**
 #
+# For all other applications use a preexisting class that include `Crypto::Secret`
 @[Experimental]
 module Crypto::Secret
   abstract def to_slice : Bytes
@@ -8,14 +10,26 @@ module Crypto::Secret
   def readwrite
   end
 
+  def readwrite
+    yield
+  end
+
   def readonly
+  end
+
+  def readonly
+    yield
   end
 
   def noaccess
   end
 
+  def noaccess
+    yield
+  end
+
   def wipe
-    # Todo: implement wiping
+    # Todo: implement wiping.  Needs crystal support
   end
 
   def wipe
